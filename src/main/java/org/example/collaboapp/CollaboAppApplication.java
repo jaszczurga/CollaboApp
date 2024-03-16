cab7b01dc843ef5d69f5a207a5800702d04698eb
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-03-16 20:17:08 +0100

    init

---
+++ src/main/java/org/example/collaboapp/CollaboAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.collaboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( CollaboAppApplication.class , args );
+    }
+
+}
