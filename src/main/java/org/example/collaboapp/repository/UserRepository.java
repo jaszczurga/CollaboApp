2db66e4efebbd5bea72d01a354c39bc5df64314c
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-03-17 12:35:30 +0100

    done security configuration with necessary filters

---
+++ src/main/java/org/example/collaboapp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.collaboapp.repository;
+
+import org.example.collaboapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String username);
+
+}
